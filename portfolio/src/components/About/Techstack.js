8214ffc2fed6f85ef8a7386839c57d10b5032242
Edvaldo <edvaldovitor250@gmail.com>
2025-04-09 18:39:57 -0300

    add: include Selenium icon in technology stack

--- portfolio/src/components/About/Techstack.js
+++ portfolio/src/components/About/Techstack.js
@@ -1,69 +1,72 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-    DiJavascript1,
-    DiReact,
-    DiNodejs,
-    DiMongodb,
-    DiPython,
-    DiGit,
-    DiJava,
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
 } from "react-icons/di";
 import {
-    SiRedis,
-    SiPostgresql,
-    SiCsharp,
-    SiSpringboot,
-    SiTypescript,
-    SiMysql,
-    SiTailwindcss
+  SiRedis,
+  SiPostgresql,
+  SiCsharp,
+  SiSpringboot,
+  SiTypescript,
+  SiMysql,
+  SiTailwindcss,
+  SiSelenium
 } from "react-icons/si";
 
 function Techstack() {
-    return (
-        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-
-            <Col xs={4} md={2} className="tech-icons">
-                <SiCsharp />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <DiJavascript1 />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <SiTypescript />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <DiPython />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <DiJava />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <SiSpringboot />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <DiNodejs />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <DiReact />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <SiTailwindcss />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <DiMongodb />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <SiMysql />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <SiPostgresql />
-            </Col>
-            <Col xs={4} md={2} className="tech-icons">
-                <SiRedis />
-            </Col>
-        </Row>
-    );
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCsharp />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTypescript />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpringboot />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedis />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSelenium />
+      </Col>
+    </Row>
+  );
 }
 
 export default Techstack;
