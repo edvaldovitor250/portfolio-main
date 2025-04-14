0343ad043c08c8ccf2a98a815a1832c6f108cfcb
Edvaldo <edvaldovitor250@gmail.com>
2025-04-13 22:07:43 -0300

    refactor(components): extract tech and tool stack icons to data files for better modularity

---
+++ portfolio/src/data/techStack.js
@@ -0,0 +1,38 @@
+import {
+    DiJavascript1,
+    DiReact,
+    DiNodejs,
+    DiMongodb,
+    DiPython,
+    DiJava,
+  } from "react-icons/di";
+  import {
+    SiRedis,
+    SiPostgresql,
+    SiCsharp,
+    SiSpringboot,
+    SiTypescript,
+    SiMysql,
+    SiTailwindcss,
+    SiSelenium,
+  } from "react-icons/si";
+  
+  const techStack = [
+    SiCsharp,
+    DiJavascript1,
+    SiTypescript,
+    DiPython,
+    DiJava,
+    SiSpringboot,
+    DiNodejs,
+    DiReact,
+    SiTailwindcss,
+    DiMongodb,
+    SiMysql,
+    SiPostgresql,
+    SiRedis,
+    SiSelenium,
+  ];
+  
+  export default techStack;
+  
---
+++ portfolio/src/data/toolStack.js
@@ -0,0 +1,22 @@
+import {
+    SiVisualstudiocode,
+    SiPostman,
+    SiIntellijidea,
+    SiDocker,
+    SiGit,
+    SiSonarqube,
+    SiPycharm,
+  } from "react-icons/si";
+  
+  const toolStack = [
+    SiIntellijidea,
+    SiPycharm,
+    SiVisualstudiocode,
+    SiPostman,
+    SiDocker,
+    SiGit,
+    SiSonarqube,
+  ];
+  
+  export default toolStack;
+  
