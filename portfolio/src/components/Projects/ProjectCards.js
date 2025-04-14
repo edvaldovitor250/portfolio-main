74f1e93748fe9b86f3dd081840b56a9dae7c308c
Edvaldo <edvaldovitor250@gmail.com>
2025-04-13 21:00:32 -0300

    refactor: convert ProjectCards to const component and improve readability

--- portfolio/src/components/Projects/ProjectCards.js
+++ portfolio/src/components/Projects/ProjectCards.js
@@ -4,36 +4,46 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
-function ProjectCards(props) {
+const ProjectCards = ({ imgPath, title, description, ghLink, isBlog, demoLink }) => {
+  const hasGitHubLink = Boolean(ghLink);
+  const hasDemoLink = Boolean(demoLink);
+
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={imgPath} alt={title} />
+
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title>{title}</Card.Title>
+
         <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
+          {description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
 
-
-        {!props.isBlog && props.demoLink && (
+        {hasGitHubLink && (
           <Button
             variant="primary"
-            href={props.demoLink}
+            href={ghLink}
             target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsGithub /> &nbsp; {isBlog ? "Blog" : "GitHub"}
+          </Button>
+        )}
+
+        {hasDemoLink && !isBlog && (
+          <Button
+            variant="primary"
+            href={demoLink}
+            target="_blank"
+            rel="noopener noreferrer"
             style={{ marginLeft: "10px" }}
           >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+            <CgWebsite /> &nbsp; Demo
           </Button>
         )}
       </Card.Body>
     </Card>
   );
-}
+};
+
 export default ProjectCards;
